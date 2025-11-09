86f132851795abd1d8111e1a596184a198c9a773
Rubelito Abella <bbadoodles@gmail.com>
2025-11-10 00:17:23 +0800

    update dataset with new distributions

--- Exams/Midterms2025.md
+++ Exams/Midterms2025.md
@@ -12,9 +12,8 @@
 
 ## Data Cleaning
 
-1. Some of the columns in the dataset have missing values. Check which columns contain missing elements. Take note of the number of missing values for each column.
-2. Delete rows that contain more than one missing value.
-3. Impute the remaining missing values with the mean (quantitative) and mode (qualitative) of their corresponding column.
+1. Some of the features in the dataset have missing values. Check which columns contain missing elements. Take note of the number of missing values for each column.
+3. Impute the missing values with the mean (quantitative) and mode (qualitative) of their corresponding column.
 
 ## Data Processing
 
@@ -27,24 +26,22 @@
 1. Create a new dataset with a larger granularity. In this dataset, rows from "extended_public_transportation.csv" with the same `origin_station` are summarized into one row.
 2. The new dataset will have the following columns:
     - `origin_station` - qualitative, the corresponding origin station of the row.
-    - `weather_condition` - qualitative, the weather condition of that specific date. (use mode to summarize)
-    - `mean_temperature_C` - quantitative, the mean temperature of all rows under the corresponding date. Keep the unit as Celcius.
-    - `mean_humidity_percent` - quantitative, the mean humidity percentage of all rows under the corresponding date.
-    - `mean_wind_speed_kmh` - quantitative, the mean wind speed of all rows under the corresponding date. Keep the unit as kmh.
-    - `mean_precipitation_mm` - quantitative, the mean precipitation of all rows under the corresponding date. Keep the unit as mm.
-    - `number_of_delays` - quantitative, the total number of delays for the corresponding date.
+    - `weather_condition` - qualitative, the weather condition of that specific station. (use mode to summarize)
+    - `mean_temperature_C` - quantitative, the mean temperature of all rows of the corresponding station. Keep the unit as Celcius.
+    - `mean_humidity_percent` - quantitative, the mean humidity percentage of all rows of the corresponding station.
+    - `mean_wind_speed_kmh` - quantitative, the mean wind speed of all rows of the corresponding station. Keep the unit as kmh.
+    - `mean_precipitation_mm` - quantitative, the mean precipitation of all rows of the corresponding station. Keep the unit as mm.
 3. Export this new dataset as a .csv file. The file name of the exported file should be "daily_public_transportation_delays.csv"
 
 ## Declarative statistics
 
 1. From "extended_public_transportation.csv", find the mean, median, and standard_deviation of the following quantitative columns: `temperature_C`, `humidity_percent`, `wind_speed_kmh`, `precipitation_mm`.
-2. From "daily_public_transportation_delays.csv", find the mean, median, and standard deviation of `number_of_delays`.
 
 ## Insights from visualization
 
-1. Create a box plot comparing the `actual_arrival_delay_min` of all `weather_conditions`, take not which weather conditions have the highest medians for `actual_arrival_delay_min`
-2. Create a scatter plot comparing the features `mean_precipitation_mm` and `actual_arrival_delay_min`, assess whether the these two variables are positively correlated, negatively correlated, or not correlated at all. Place `mean_precipitation_mm` in the x-axis if the plot and `actual_arrival_delay_min` in the y-axis of the plot.
+1. Create a box plot comparing the `actual_arrival_delay_min` of all `weather_conditions`, take note which weather conditions have the highest medians for `actual_arrival_delay_min`.
+2. Create a scatter plot comparing the features `precipitation_mm` and `actual_arrival_delay_min`. 
 3. Export the box plot as an image and save the image as "box_plot_delays_vs_weather.png".
-4. Export the scatter plot as an image annd save the image a s "scatter_plot_precipitation_vs_delay.png"
+4. Export the scatter plot as an image and save the image a s "scatter_plot_precipitation_vs_delay.png"
 
 ## Grading Rubric
--- prepareData.py
+++ prepareData.py
@@ -23,36 +23,42 @@ def main():
     data['scheduled_departure'] = pandas.to_datetime(data['date'] + " " + data['time'], format='%Y-%m-%d %H:%M:%S')
     data['peak_hour'] = data['scheduled_departure'].dt.hour.apply(lambda hour: 1 if (hour == 8 or hour == 18) else 0)
 
-    data['delay_offset'] = -10
+    data['actual_arrival_delay_min'] = pandas.Series(np.round(rng.normal(loc=0, scale=3, size=data.shape[0])), index=data.index)
+    data['temperature_C'] = pandas.Series(np.round(rng.normal(loc=25, scale=5, size=data.shape[0]), decimals=2), index=data.index)
+    data['humidity_percent'] = pandas.Series(np.round(rng.normal(loc=53, scale=5, size=data.shape[0]), decimals=2), index=data.index)
+    data['precipitation_mm'] = pandas.Series(np.maximum(np.round(rng.normal(loc=2, scale=0.3, size=data.shape[0]), decimals=2), 0), index=data.index)
+    data['wind_speed_kmh'] = pandas.Series(np.maximum(np.round(rng.normal(loc=15, scale=5, size=data.shape[0]), decimals=2), 0), index=data.index)
 
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'weather_condition', 'Rain', 2, 2)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'weather_condition', 'Storm', 7, 3)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'weather_condition', 'Snow', 7, 1)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'event_type', 'Concert', 3, 2)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'event_type', 'Parade', 4, 1)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'event_type', 'Festival', 5, 1)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'event_type', 'Sports', 2, 1)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'peak_hour', 1, 6, 1)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'transport_type', 'Bus', 4, 7)
+    data['delay_offset'] = -3
+
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'weather_condition', 'Rain', 4, 2)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'weather_condition', 'Storm', 15, 3)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'weather_condition', 'Snow', 9, 1)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'event_type', 'Concert', 7, 2)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'event_type', 'Parade', 9, 3)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'event_type', 'Festival', 8, 2)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'event_type', 'Sports', 4, 1)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'peak_hour', 1, 7, 2)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'transport_type', 'Bus', 8, 10)
     data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'transport_type', 'Tram', 1, 2)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'origin_station', 'Station_9', 3, 2)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'origin_station', 'Station_10', 6, 1)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'origin_station', 'Station_11', 4, 1)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'origin_station', 'Station_12', -5, 4)
-    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'destination_station', 'Station_13', 10, 6)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'origin_station', 'Station_9', 6, 2)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'origin_station', 'Station_10', 7, 1)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'origin_station', 'Station_11', 9, 4)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'origin_station', 'Station_12', -7, 4)
+    data['delay_offset'] = data['delay_offset'] + normalOffsets(data, 'destination_station', 'Station_13', 15, 4)
 
     data['humidity_percent'] = data['humidity_percent'] + normalOffsets(data, 'weather_condition', 'Snow', -5, 1)
-    data['temperature_C'] = data['temperature_C'] + normalOffsets(data, 'weather_condition', 'Snow', -11, 1)
-    data['temperature_C'] = data['temperature_C'] + normalOffsets(data, 'weather_condition', 'Storm', -6, 2)
+    data['temperature_C'] = data['temperature_C'] + normalOffsets(data, 'weather_condition', 'Snow', -20, 3)
+    data['temperature_C'] = data['temperature_C'] + normalOffsets(data, 'weather_condition', 'Storm', -13, 4)
     data['temperature_C'] = data['temperature_C'] + normalOffsets(data, 'weather_condition', 'Rain', -5, 1)
     data['temperature_C'] = data['temperature_C'] + normalOffsets(data, 'weather_condition', 'Cloudy', -3, 1)
-    data['precipitation_mm'] = data['precipitation_mm'] + normalOffsets(data, 'weather_condition', 'Snow', 10, 1)
-    data['precipitation_mm'] = data['precipitation_mm'] + normalOffsets(data, 'weather_condition', 'Storm', 15, 7)
-    data['precipitation_mm'] = data['precipitation_mm'] + normalOffsets(data, 'weather_condition', 'Rain', 9, 4)
-    data['wind_speed_kmh'] = data['wind_speed_kmh'] + normalOffsets(data, 'weather_condition', 'Storm', 25, 5)
-    data['precipitation_mm'] = data.apply(lambda row: rng.normal(loc = 1.0, scale = 2.0) if row['weather_condition'] == 'Clear' else row['precipitation_mm'], axis = 1)
-    data['precipitation_mm'] = data.apply(lambda row: rng.normal(loc = 2.0, scale = 3.0) if row['weather_condition'] == 'Fog' else row['precipitation_mm'], axis = 1)
-    data['precipitation_mm'] = data.apply(lambda row: rng.normal(loc = 2.5, scale = 3.0) if row['weather_condition'] == 'Cloudy' else row['precipitation_mm'], axis = 1)
+    data['precipitation_mm'] = data['precipitation_mm'] + normalOffsets(data, 'weather_condition', 'Snow', 13, 1)
+    data['precipitation_mm'] = data['precipitation_mm'] + normalOffsets(data, 'weather_condition', 'Storm', 19, 7)
+    data['precipitation_mm'] = data['precipitation_mm'] + normalOffsets(data, 'weather_condition', 'Rain', 4, 4)
+    data['wind_speed_kmh'] = data['wind_speed_kmh'] + normalOffsets(data, 'weather_condition', 'Storm', 30, 5)
+  #  data['precipitation_mm'] = data.apply(lambda row: rng.normal(loc = 1.0, scale = 2.0) if row['weather_condition'] == 'Clear' else row['precipitation_mm'], axis = 1)
+  #  data['precipitation_mm'] = data.apply(lambda row: rng.normal(loc = 2.0, scale = 3.0) if row['weather_condition'] == 'Fog' else row['precipitation_mm'], axis = 1)
+  #  data['precipitation_mm'] = data.apply(lambda row: rng.normal(loc = 2.5, scale = 3.0) if row['weather_condition'] == 'Cloudy' else row['precipitation_mm'], axis = 1)
 
     data['actual_arrival_delay_min'] = data['actual_arrival_delay_min'] + data['delay_offset']
 
@@ -82,8 +88,7 @@ def main():
         "event_type",
         "peak_hour",
     ]]
-
-    reducedData.to_csv("assets/Datasets/public_transportation_delays.csv")
+    reducedData.to_csv("assets/Datasets/public_transportation_delays.csv", index=False)
 
 if __name__ == "__main__":
     main()
